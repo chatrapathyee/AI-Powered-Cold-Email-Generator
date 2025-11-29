b849c435e57e2ed1315457902f1405cfb122f49a
chatrapathyee <chatrapatheerathod369@gmail.com>
2025-11-29 19:41:16 +0530

    third changes

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from dotenv import load_dotenv
 import streamlit as st
 from langchain_community.document_loaders import WebBaseLoader
 
+# Fix for ChromaDB on Streamlit Cloud
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from chains import Chain
 from portfolio import Portfolio
 from utils import clean_text
